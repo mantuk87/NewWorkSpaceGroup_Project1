2e6cb4e044f0751079ad7336dce6e049d44e69fb
mantuk87 <mantu.montu1987@gmail.com>
2020-05-28 19:44:31 +0530

    Added new class i.e Class1

---
+++ src/main/java/com/newWorkspace/qa/Login/Class1.java
@@ -0,0 +1,10 @@
+package com.newWorkspace.qa.Login;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+	@Test
+	public void class1Test1() {
+		System.out.println("Test1 method of Class1");
+	}
+}
